d0427f7d70abab22cbad93058547a124c5253f23
adsjinf <jinfrj@hotmail.com>
2023-07-24 17:47:20 -0300

    Implementado o \n e removendo println desnecessário

--- src/Modulo1_4/TesteContaCorrente.java
+++ src/Modulo1_4/TesteContaCorrente.java
@@ -11,21 +11,17 @@ public class TesteContaCorrente {
         MovimentacaoDeContaCorrente contaCorrente = new MovimentacaoDeContaCorrente();
         System.out.println("******************************************************************");
         System.out.println("Dados iniciais do cliente: ");
-        System.out.println();
-        System.out.println("Nome:          " + contaCorrente.getNome());
+        System.out.println("\nNome:          " + contaCorrente.getNome());
         System.out.println("Tipo conta:    " + contaCorrente.getTipoConta());
         System.out.println("Saldo inicial: " + contaCorrente.getSaldo());
-        System.out.println("******************************************************************");
+        System.out.println("\n******************************************************************");
         while (operacao!=4){
-            System.out.println();
-            System.out.println("Operações");
-            System.out.println();
-            System.out.println("1 - Consulta saldos");
+            System.out.println("\nOperações");
+            System.out.println("\n1 - Consulta saldos");
             System.out.println("2 - Receber valor");
             System.out.println("3 - Transferir valor");
             System.out.println("4 - Sair");
-            System.out.println();
-            System.out.println("Digite a opção desejada:");
+            System.out.println("\nDigite a opção desejada:");
             operacao = leitura.nextInt();
             if (operacao == 1){
                 System.out.println("O saldo da conta é: " + contaCorrente.getSaldo());
